e6a98072d83af40c917382dd7e2ebfd67aaf16b1
Richard Hoofring <rhoofr@yahoo.com>
2022-06-26 17:35:52 -0400

    test continuous deployment

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
+        <h2>Testing continuous deployment</h2>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
